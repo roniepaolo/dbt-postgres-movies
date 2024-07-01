45e588d0fde1d797c4d3d075e54f65434459688c
Ronie Arauco <ronie.arauco@pucp.edu.pe>
2024-07-01 11:45:21 -0500

    Test no description 101

--- validations.sh
+++ validations.sh
@@ -17,39 +17,40 @@ models_files=("$@")
 
 # Validations
 for file in "${models_files[@]}"; do
-	echo "$file"
-	# echo "Check mandatory fields (name, description, and columns)"
-	# yq eval \
-	# 	'["name", "description", "columns"] - (.models.[0] | keys) | length == 0' \
-	# 	"$file" --exit-status --no-doc
-	#
-	# echo "Check that the name is not empty"
-	# yq eval \
-	# 	'.models.[0].name | length > 0' \
-	# 	"$file" --exit-status --no-doc
-	#
-	# echo "Check that the description is not empty"
-	# yq eval \
-	# 	'.models.[0].description | length > 0' \
-	# 	"$file" --exit-status --no-doc
-	#
-	# echo "Check if columns have a name"
-	# yq eval \
-	# 	'[.models.[0].columns[] | has("name")] | all' \
-	# 	"$file" --exit-status --no-doc
-	#
-	# echo "Check if columns have a description"
-	# yq eval \
-	# 	'[.models.[0].columns[] | has("description")] | all' \
-	# 	"$file" --exit-status --no-doc
-	#
-	# echo "Check that column names are not empty"
-	# yq eval \
-	# 	'.models.[0].columns | all_c(.name | length > 0)' \
-	# 	"$file" --exit-status --no-doc
-	#
-	# echo "Check that column descriptions are not empty"
-	# yq eval \
-	# 	'.models.[0].columns | all_c(.description | length > 0)' \
-	# 	"$file" --exit-status --no-doc
+	echo "--"
+	echo "Validation for $file"
+	echo "Check mandatory fields (name, description, and columns)"
+	yq eval \
+		'["name", "description", "columns"] - (.models.[0] | keys) | length == 0' \
+		"$file" --exit-status --no-doc
+
+	echo "Check that the name is not empty"
+	yq eval \
+		'.models.[0].name | length > 0' \
+		"$file" --exit-status --no-doc
+
+	echo "Check that the description is not empty"
+	yq eval \
+		'.models.[0].description | length > 0' \
+		"$file" --exit-status --no-doc
+
+	echo "Check if columns have a name"
+	yq eval \
+		'[.models.[0].columns[] | has("name")] | all' \
+		"$file" --exit-status --no-doc
+
+	echo "Check if columns have a description"
+	yq eval \
+		'[.models.[0].columns[] | has("description")] | all' \
+		"$file" --exit-status --no-doc
+
+	echo "Check that column names are not empty"
+	yq eval \
+		'.models.[0].columns | all_c(.name | length > 0)' \
+		"$file" --exit-status --no-doc
+
+	echo "Check that column descriptions are not empty"
+	yq eval \
+		'.models.[0].columns | all_c(.description | length > 0)' \
+		"$file" --exit-status --no-doc
 done
