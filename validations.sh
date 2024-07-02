01bc2cfa559c7a90425c777f8d508bd9fc5c867e
Ronie Arauco <ronie.arauco@pucp.edu.pe>
2024-07-02 11:18:24 -0500

    Test no description 102

--- validations.sh
+++ validations.sh
@@ -1,56 +1,46 @@
 #!/bin/bash
 
-# This bash script performs validations in dbt models.
+# This Bash script performs validations in dbt models.
 # It ensures that the specified fields in the YAML structure meet certain
 # conditions.
 
 set -euo xtrace
 
 # Only run when models exist
-if [ "$#" -lt 1 ]; then
-	echo "No models read."
-	echo "Usage: $0 <models_files>..."
+if [[ $# -lt 1 ]]; then
+	echo 1>&2 "No models read."
+	echo 1>&2 "Usage: $0 <models_files>..."
 	exit 1
 fi
 
-models_files=("$@")
+check-file() {
+	local input=$1
+	local title=$2
+	local yqexpr=$3
 
-# Validations
-for file in "${models_files[@]}"; do
-	echo "--"
-	echo "Validation for $file"
-	echo "Check mandatory fields (name, description, and columns)"
-	yq eval \
-		'["name", "description", "columns"] - (.models.[0] | keys) | length == 0' \
-		"$file" --exit-status --no-doc
+	printf '%s\n' "$title"
+	yq eval "$yqexpr" "$input" --exit-status --no-doc
+}
 
-	echo "Check that the name is not empty"
-	yq eval \
-		'.models.[0].name | length > 0' \
-		"$file" --exit-status --no-doc
+for f in "$@"; do
+	check-file "$f" "Check mandatory fields (name, description, and columns)" \
+		'["name", "description", "columns"] - (.models.[0] | keys) | length == 0'
 
-	echo "Check that the description is not empty"
-	yq eval \
-		'.models.[0].description | length > 0' \
-		"$file" --exit-status --no-doc
+	check-file "$f" "Check that the name is not empty" \
+		'.models.[0].name | length > 0'
 
-	echo "Check if columns have a name"
-	yq eval \
-		'[.models.[0].columns[] | has("name")] | all' \
-		"$file" --exit-status --no-doc
+	check-file "$f" "Check that the description is not empty" \
+		'.models.[0].description | length > 0'
 
-	echo "Check if columns have a description"
-	yq eval \
-		'[.models.[0].columns[] | has("description")] | all' \
-		"$file" --exit-status --no-doc
+	check-file "$f" "Check if columns have a name" \
+		'[.models.[0].columns[] | has("name")] | all'
 
-	echo "Check that column names are not empty"
-	yq eval \
-		'.models.[0].columns | all_c(.name | length > 0)' \
-		"$file" --exit-status --no-doc
+	check-file "$f" "Check if columns have a description" \
+		'[.models.[0].columns[] | has("description")] | all'
 
-	echo "Check that column descriptions are not empty"
-	yq eval \
-		'.models.[0].columns | all_c(.description | length > 0)' \
-		"$file" --exit-status --no-doc
+	check-file "$f" "Check that column names are not empty" \
+		'.models.[0].columns | all_c(.name | length > 0)'
+
+	check-file "$f" "Check that column descriptions are not empty" \
+		'.models.[0].columns | all_c(.description | length > 0)'
 done
